9ed19036c29265591d758ca561bf6e7f27eeb91a
Benjamin LECOMTE <benjamin.lecomte@etu.esisar.grenoble-inp.fr>
2019-11-20 16:16:55 +0100

    plop

--- decodage.c
+++ decodage.c
@@ -2,6 +2,7 @@
 
 void lectureFichier(const char* fichierEntree)
 {
+	//Ouvre le fichier d'entrée, traduit chacune des lignes en hexadécimale
 	int lecture;
 	size_t taille = 20;
 	char* ligne = (char *)malloc(taille * sizeof(char));
@@ -11,5 +12,29 @@ void lectureFichier(const char* fichierEntree)
 	{
         printf("Ligne : %s", ligne);
         //ON LECRIT EN HEXA
-    }	
+    }
+    fclose(fichier);	
+}
+
+char* conversionBinaire(const int aConvertir, char* binaire)
+{
+	//Convertit une valeur < à 256 en binaire
+	int i;
+    for(i = 7; i >= 0; --i)
+    {
+        sprintf(binaire+(7-i),"%c",(aConvertir & (1 << i)) ? '1' : '0');
+    }
+	binaire[(7-i)] = '\0';
+	return binaire;
+}
+
+char* operande(const char* instruction, int rangOperande)
+//A partir d'une instruction, donne l'opérande indiquée
+{
+	char* operande[20];
+	int fin = 0;
+	int i = 0;
+	int nombreOperande = 0;
+	operande = strchr(instruction, ' ');
+	//PAS FINI
 }
--- decodage.h
+++ decodage.h
@@ -3,3 +3,5 @@
 #include <string.h>
 
 void lectureFichier(const char* fichierEntree);
+char* conversionBinaire(const int aConvertir, char* binaire);
+char* operande(const char* instruction, int rangOperande);
