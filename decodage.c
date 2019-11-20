f8c1a3c14dc1752f18fb6f8f077fb0876ba07d36
Benjamin LECOMTE <benjamin.lecomte@etu.esisar.grenoble-inp.fr>
2019-11-20 15:44:38 +0100

    Lecture du fichier d'entrée

--- decodage.c
+++ decodage.c
@@ -2,5 +2,14 @@
 
 void lectureFichier(const char* fichierEntree)
 {
-	
+	int lecture;
+	size_t taille = 20;
+	char* ligne = (char *)malloc(taille * sizeof(char));
+	FILE* fichier = fopen(fichierEntree, "r");
+	if(fichier == NULL) perror("Problème lors de l'ouverture du fichier");
+	while((lecture = getline(&ligne, &taille, fichier)) != -1) 
+	{
+        printf("Ligne : %s", ligne);
+        //ON LECRIT EN HEXA
+    }	
 }
--- main.c
+++ main.c
@@ -2,5 +2,6 @@
 
 int main(int argc, char const *argv[])
 {
+	lectureFichier("tests/in1.txt");
 	return 0;
 }
