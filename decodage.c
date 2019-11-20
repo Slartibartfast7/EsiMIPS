37c3f129d45dbb91f8a6ed0b0ad445e25fa561e5
Slartibartfast7 <thibaut@meteors.io>
2019-11-20 16:20:38 +0100

    Debut de decodeInstruction()

--- decodage.c
+++ decodage.c
@@ -1,6 +1,35 @@
 #include "decodage.h"
 
+char *decodeInstruction(char *bufEntree)
+{
+	int i;
+	char *bufptrEntree = bufEntree;
+	char bufptrSortie[33];
+	char opcode[8];
+	char *operandes[3];
+
+	// Obtention de l'OPCODE
+	i = 0;
+	while (bufptr != ' ')
+	{
+		opcode[i] = bufptr;
+		bufptr++;
+		i++;
+	}
+	opcode[i] = '\0';
+
+	i = 0;
+	while (!strcmp(BIN_OPCODES[i], opcode))
+	{
+		i++;
+	}
+
+	bufptrSortie = *FCT_OPCODES[i](bufEntree);
+
+	return bufptrSortie;
+}
+
 void lectureFichier(const char* fichierEntree)
 {
-	
+
 }
--- decodage.h
+++ decodage.h
@@ -1,3 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+char *BIN_OPCODES[] = {"ADD", "ADDI", "AND", "BEQ", "BGTZ", "BLEZ", "BNE", "DIV", "J", "JAL", "JR", "LUI", "LW", "MFHI", "MFLO", "MULT", "NOP", "OR", "ROTR", "SLL", "SLT", "SRL", "SUB", "SW", "SYSCALL", "XOR"};
+void (*FCT_OPCODES[])(const *char) = {decode_add, decode_addi, decode_and, decode_beq, decode_bgtz, decode_blez, decode_bne, decode_div, decode_j, decode_jal, decode_jr, decode_lui, decode_lw, decode_mfhi, decode_mflo, decode_mult, decode_nop, decode_or, decode_rotr, decode_sll, decode_slt, decode_srl, decode_sub, decode_sw, decode_syscall, decode_xor};
